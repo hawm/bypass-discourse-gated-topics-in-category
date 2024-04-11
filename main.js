1cafed9a1a0f25c21a548a1e8e5c29ffad556f82
hawm <20796940+hawm@users.noreply.github.com>
2024-04-11 11:36:25 +0800

    check Discourse before bypass apply

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 // ==UserScript==
 // @name   bypass discourse-gated-topics-in-category
 // @namespace   Violentmonkey Scripts
-// @match       *://*/t/*/*
 // @grant       none
-// @version     1.1
+// @version     1.2
 // @author      hawm
 // @description A userscript to bypass the discourse-gated-topics-in-category theme component
 // @require https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
@@ -11,16 +10,29 @@
 
 const gatedClassName = 'topic-in-gated-category';
 
-function bypass(){
-  console.log(document.body.classList)
-  if(document.body.classList.contains(gatedClassName)){
-    // clear style
-    document.body.classList.remove(gatedClassName);
-    // hide banner
-    document.querySelector(`.topic-above-post-stream-outlet.${gatedClassName}`).style.display = 'none';
-  }
+function isGatedTopic(){
+  return document.body.classList.contains(gatedClassName);
 }
 
-const observer = new MutationObserver(bypass);
+function bypassGated(){
+  // clear style
+  document.body.classList.remove(gatedClassName);
+  // hide banner
+  document.querySelector(`.topic-above-post-stream-outlet.${gatedClassName}`).style.display = 'none';
+}
 
-observer.observe(document.body, {attributeFilter: ['class']})
+function bypass(){
+  isGatedTopic() && bypassGated();
+}
+
+const observer = new MutationObserver((record, observer)=>{
+  bypass();
+});
+
+// apply on Discourse powered website only
+if(Discourse){
+  // for init page
+  bypass();
+  // for page change
+  observer.observe(document.body, {attributeFilter: ['class']});
+}
